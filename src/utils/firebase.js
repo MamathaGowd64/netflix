48406368564c4fd66e2e7f504d8baa262a60259d
MamathaGowd64 <mamatha.gowd2020@gmail.com>
2024-08-14 14:45:44 +0530

    project deployed on firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"
@@ -8,13 +9,13 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyX-5hcy5mQu7FYDCHo_hjODWIIu5Vbf4",
-  authDomain: "netflix-b3de0.firebaseapp.com",
-  projectId: "netflix-b3de0",
-  storageBucket: "netflix-b3de0.appspot.com",
-  messagingSenderId: "143113403118",
-  appId: "1:143113403118:web:d69b160c7da724f8a5d1ff",
-  measurementId: "G-ZMJMLG3X11"
+  apiKey: "AIzaSyCT9JuGCtjMGStVBLax9hBweqRquRcI6aM",
+  authDomain: "netflix-47b3b.firebaseapp.com",
+  projectId: "netflix-47b3b",
+  storageBucket: "netflix-47b3b.appspot.com",
+  messagingSenderId: "154422334249",
+  appId: "1:154422334249:web:18966c41d47a6b8be85d38",
+  measurementId: "G-KPTMTFB8S5"
 };
 
 // Initialize Firebase
